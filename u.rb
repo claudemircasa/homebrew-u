5b15746713867668656aa30b9d033f33170ea484
Claudemir Casa <claudemir.casa@governor.ltd>
2026-05-09 13:25:54 -0300

    feat(runtime): implemented runtime evaluation for Unary Expressions ('not' and '-')

--- u.rb
+++ u.rb
@@ -1,10 +1,10 @@
 class U < Formula
   desc "Universal Abstract Language (U) Compiler & U-Shell"
   homepage "https://governor.ltd"
-  url "https://github.com/claudemircasa/u/releases/download/v1.1.0/u-mac-v1.1.0.tar.gz"
-  sha256 "6183578cdfcfb1a751c05a903302e127352d1553948f3be0a5c58d5bcf64d773"
-  version "1.1.0"
-  license "MIT"
+  url "https://github.com/claudemircasa/u/releases/download/v1.1.1/u-mac-v1.1.1.tar.gz"
+  sha256 "c16038b692e8fbc59f58d8dae8dc025b0f93cea232ea77c2cd00decfb481175c"
+  version "1.1.1"
+  # Proprietary license: Governor Ltda.
 
   def install
     bin.install "u"
