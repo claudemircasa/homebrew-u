02f306d70491430d482c0c6daf310dafa8351bc5
Claudemir Casa <claudemir.casa@governor.ltd>
2026-05-09 13:40:48 -0300

    updates

--- u.rb
+++ u.rb
@@ -1,7 +1,7 @@
 class U < Formula
   desc "Universal Abstract Language (U) Compiler & U-Shell"
   homepage "https://governor.ltd"
-  url "https://github.com/claudemircasa/u-releases/releases/download/v1.1.1/u-mac-v1.1.1.tar.gz"
+  url "https://github.com/claudemircasa/homebrew-u/releases/download/v1.1.1/u-mac-v1.1.1.tar.gz"
   sha256 "c16038b692e8fbc59f58d8dae8dc025b0f93cea232ea77c2cd00decfb481175c"
   version "1.1.1"
   # Proprietary license: Governor Ltda.
